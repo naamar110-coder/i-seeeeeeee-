9f5881bbb61dccea16697192a87f2d9d167bb095
naamar110-coder <naamar110@gmail.com>
2025-09-14 00:41:43 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,12 @@
+// firebase-config.js
+
+// Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyBaUdPEqcwk66PI-2A_Q8UmH5...",
+  apiKey: "AIzaSyBaUdPEqecwk66PI-2A_Q8UmH56F21YzQ0",
   authDomain: "i-seeeeeeee.firebaseapp.com",
   projectId: "i-seeeeeeee",
-  storageBucket: "i-seeeeeeee.appspot.com",
+  storageBucket: "i-seeeeeeee.firebasestorage.app",
   messagingSenderId: "870829131842",
-  appId: "1:870829131842:web:def3d582f8ea9...",
-  measurementId: "G-22GWKZMZ6L"
+  appId: "1:870829131842:web:4288d9bdaa8b988d3145db",
+  measurementId: "G-FXLD86D6ZW"
 };
