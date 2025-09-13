739727e2535f9511e6fe404c5377e54f77d6634f
naamar110-coder <naamar110@gmail.com>
2025-09-13 23:41:02 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBaUdPEqcwk66PI-2A_Q8UmH5...",
+  authDomain: "i-seeeeeeee.firebaseapp.com",
+  projectId: "i-seeeeeeee",
+  storageBucket: "i-seeeeeeee.appspot.com",
+  messagingSenderId: "870829131842",
+  appId: "1:870829131842:web:def3d582f8ea9...",
+  measurementId: "G-22GWKZMZ6L"
+};
